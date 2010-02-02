6704649442ca0e9af426e92592a212659c110032
Blake Smith <blakesmith0@gmail.com>
2010-02-02 13:38:12 -0600

    Initial commit

---
+++ instapaper.pl
@@ -0,0 +1,63 @@
+use strict;
+use LWP::UserAgent;
+use HTTP::Request::Common qw(POST);
+use Irssi;
+use Irssi::Irc;
+use vars qw($VERSION %IRSSI);
+
+$VERSION = "0.1";
+%IRSSI   = (
+    authors     => 'Blake Smith',
+    contact     => 'blakesmith0@gmail.com',
+    name        => 'instapaper',
+    description => 'Post urls to your instapaper.com account',
+    license => 'GNU GPL v2',
+    url     => 'http://github.com/blakesmith/instapaper.pl',
+    #  changed => '$Date: 2009-08-07 01:24:53 -0700 (Fri, 07 Aug 2009) $',
+);
+
+my $add_location = "https://www.instapaper.com/api/add";
+my $ua = new LWP::UserAgent;
+
+sub cmd_instapaper {
+  my ($data, $server, $window) = @_;
+
+  my $username = Irssi::settings_get_str("instapaper_username");
+  my $password = Irssi::settings_get_str("instapaper_password");
+
+  unless ($username) {
+    window_echo($window, "Please set a username with /set instapaper_username <USERNAME>
+        and an optional password with /set instapaper_password <PASSWORD>");
+    return;
+  }
+
+  my $req = POST $add_location,
+              [ 
+                username => $username, 
+                password => $password,
+                url => $data
+              ];
+  my $response = $ua->request($req);
+
+  window_echo($window, echo_response($response));
+}
+
+
+sub echo_response {
+  my ( $response ) = @_;
+
+  my $code = $response->code;
+  if ($code == 201) { return "Url saved!" }
+  elsif ($code == 400) { return "Bad request" }
+  elsif ($code == 403) { return "Invalid username or password, please set the instapaper_username and instapaper_password fields" }
+  elsif ($code == 500) { return "Server returned an error" }
+}
+
+sub window_echo {
+  my ($window, $message) = @_;
+  Irssi::print($message);
+}
+
+Irssi::settings_add_str("instapaper", "instapaper_username", undef );
+Irssi::settings_add_str("instapaper", "instapaper_password", undef );
+Irssi::command_bind("instapaper", "cmd_instapaper" );
